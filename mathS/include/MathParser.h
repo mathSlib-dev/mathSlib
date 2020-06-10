5a3e6b6c8ad78db04de0b7e2644bb6702d43da86
EasternJournalist <47034050+EasternJournalist@users.noreply.github.com>
2020-06-10 16:45:17 +0800

    Update parser

--- mathS/include/MathParser.h
+++ mathS/include/MathParser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <MathObject.h>
+#include <string>
 
 /*
 	In this file, we introduce methods that parse text input of expressions 
@@ -8,5 +9,27 @@
 */
 namespace mathS
 {
+	class Parser
+	{
+	public:
 
+		//
+		MathObject* Parse(const std::string& str);
+
+	private:
+		static bool inline is_figure(char c);
+		static bool inline is_op(char c);
+		static bool is_varchar(char c);
+
+		MathObject* Parse(const std::string& str, int& i, int lv);
+
+		MathObject* parse_number(const std::string& str, int& i);
+		MathObject* parse_variable(const std::string& str, int& i);
+		MathObject* parse_string(const std::string& str, int& i);
+		MathObject* parse_vector(const std::string& str, int& i);
+		MathObject* parse_function(const std::string& str, int& i);
+		MathObject* parse_list(const std::string& str, int& i);
+
+		MathObject* parse_list_forced(const std::string& str, int& i);
+	};
 }
--- mathS/src/MathParser.cpp
+++ mathS/src/MathParser.cpp
@@ -1,1 +1,92 @@
 #include <MathParser.h>
+
+using namespace mathS;
+
+
+bool mathS::Parser::is_figure(char c)
+{
+	return '0' <= c && c <= '9';
+}
+
+MathObject* mathS::Parser::Parse(const std::string& str, int& i, int lv)
+{
+	int len = str.size();
+	switch (lv)
+	{
+
+	default:
+		break;
+	}
+	return nullptr;
+}
+
+MathObject* mathS::Parser::parse_number(const std::string& str, int& i)
+{
+	int len = str.size();
+	int ibegin = i;
+	bool pointflag = false;
+	if(!is_figure(str[i]) && str[i]!='i')
+		return nullptr;
+	for (; i < len; i++)
+	{
+		if (is_figure(str[i])) 
+			continue;
+		if (is_op(str[i]))
+			return new Number(str.substr(ibegin, i-ibegin));
+		if (str[i] == '.' && pointflag)
+			break;
+		else
+			pointflag = true;
+		if (str[i] == 'i')
+		{
+			i++;
+			if (i==len || is_op(str[i]))
+				return new Number(str.substr(ibegin, i-ibegin));
+			else break;
+		}
+	}
+	if(i==len)
+		return new Number(str.substr(ibegin, i-ibegin));
+	return new ErrorObject("Parse: Bad symbol after " + str.substr(ibegin, i - ibegin));
+}
+
+MathObject* mathS::Parser::parse_variable(const std::string& str, int& i)
+{
+	int len = str.size();
+	int ibegin = i;
+	if (!is_varchar(str[i]) || is_figure(str[i]))
+		return nullptr;
+	for (i; i < len; i++)
+	{
+		if (is_varchar(str[i]))
+			continue;
+		if (is_op(str[i]))
+			return new Variable(str.substr(ibegin, i - ibegin));
+		return new ErrorObject("Parse: Bad symbol after " + str.substr(ibegin, i - ibegin));
+	}
+	if (i == len)
+		new ErrorObject("Parse: Bad symbol after " + str.substr(ibegin, i - ibegin));
+}
+
+MathObject* mathS::Parser::parse_vector(const std::string& str, int& i)
+{
+	if (str[i] != '{')
+		return nullptr;
+	int len = str.size();
+	int ibegin = i;
+	i++;
+	MathObject* vlist = parse_list_forced(str, i);
+	if (vlist == nullptr)
+		return new ErrorObject("Parse: Unexpected error in parsing. ");
+	if (vlist->GetType() == MathObject::Type::ERROR)
+		return vlist;
+	if (i>=len || str[i] != '}')
+	{
+		delete vlist;
+		return new ErrorObject("Parse: Unmatched brackets.");
+	}
+	i++;
+	Vector* ret = new Vector;
+	ret->list = dynamic_cast<ListObject*>(vlist);
+	return ret;
+}
