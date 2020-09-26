4c25b00aa1577e219d7057698499cdda446fc5f9
EasternJournalist <47034050+EasternJournalist@users.noreply.github.com>
2020-09-26 19:18:10 +0800

    Rule.cpp放错文件夹了

--- mathS/include/MathFunction.h
+++ mathS/include/MathFunction.h
@@ -8,5 +8,5 @@ namespace mathS {
 	// MathFunction : MathObject -> MathObject
 	// MathFunction 是对数学对象，运用一些规则来得到结果的
 	using MathFunction = std::function<Ptr<MathObject>(std::vector<Ptr<MathObject>>)>;
-	using 
+	
 }
